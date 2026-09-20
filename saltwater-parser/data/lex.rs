48e0dede7528905330b8114ec9394791d8d71f7c
Nick Zbinden <niklaus.zbinden@gmail.com>
2026-09-20 22:47:19 +0200

    Make lexer test lifetimes explicit

--- saltwater-parser/data/lex.rs
+++ saltwater-parser/data/lex.rs
@@ -490,12 +490,12 @@ pub(crate) mod test {
     use crate::*;
 
     /// Create a new preprocessor with `s` as the input
-    pub(crate) fn cpp(s: &str) -> PreProcessor {
+    pub(crate) fn cpp(s: &str) -> PreProcessor<'_> {
         let newline = format!("{}\n", s).into_boxed_str();
         cpp_no_newline(Box::leak(newline))
     }
     /// Create a new preprocessor with `s` as the input, but without a trailing newline
-    pub(crate) fn cpp_no_newline(s: &str) -> PreProcessor {
+    pub(crate) fn cpp_no_newline(s: &str) -> PreProcessor<'_> {
         PreProcessorBuilder::new(s).build()
     }
 
