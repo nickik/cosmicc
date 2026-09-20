ff91497713db04e7adc227a703d750323a73c991
Nick Zbinden <niklaus.zbinden@gmail.com>
2026-09-21 00:04:08 +0200

    Re-export LexError from parser error module

--- saltwater-sia/lib.rs
+++ saltwater-sia/lib.rs
@@ -21,7 +21,8 @@ use saltwater_parser::data::{
     hir::{Declaration, Expr, ExprType, Initializer, LiteralValue, Stmt, StmtType, Symbol},
     CompileError, Location, StorageClass, Type,
 };
-pub use saltwater_parser::{LexError, Opt};
+pub use saltwater_parser::data::error::LexError;
+pub use saltwater_parser::Opt;
 use target_lexicon::Triple;
 
 /// The fixed target accepted by this compiler stage.
