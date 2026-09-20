41b3d144bc37484fa360dd7ff22512fbf519b0d5
Nick Zbinden <niklaus.zbinden@gmail.com>
2026-09-20 22:41:44 +0200

    Add freestanding C headers for ext2 compatibility

---
+++ saltwater-parser/headers/sys/types.h
@@ -0,0 +1,16 @@
+#ifndef _SYS_TYPES_H
+#define _SYS_TYPES_H
+
+#include <stddef.h>
+#include <stdint.h>
+
+typedef int32_t pid_t;
+typedef uint32_t uid_t;
+typedef uint32_t gid_t;
+typedef int32_t ssize_t;
+typedef int64_t off_t;
+typedef int64_t off64_t;
+typedef uint32_t mode_t;
+typedef int64_t time_t;
+
+#endif
