eff731a34db558b044199bfc76a2a24213317d98
Nick Zbinden <niklaus.zbinden@gmail.com>
2026-09-22 13:33:33 +0200

    Make SIA32 binary operator lowering exhaustive

--- saltwater-sia/lib.rs
+++ saltwater-sia/lib.rs
@@ -2119,11 +2119,8 @@ impl<'a, 'b, 'c> FunctionLowerer<'a, 'b, 'c> {
                         let boolean = self.builder.ins().icmp(condition, left, right);
                         self.builder.ins().uextend(types::I32, boolean)
                     }
-                    _ => {
-                        return Err(unsupported(
-                            expression.location,
-                            format!("SIA32 operator lowering is not implemented for {operator:?}"),
-                        ));
+                    BinaryOp::Assign | BinaryOp::LogicalAnd | BinaryOp::LogicalOr => {
+                        unreachable!()
                     }
                 };
                 Ok(value)
